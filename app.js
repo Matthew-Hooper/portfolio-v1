2bbb84fc7dc9c86ccb6cea731a285b07eff22b15
Matthew Hooper <devmatthewhooper@outlook.com>
2020-10-22 13:27:26 -0500

    Change size of projects in mobile view and made menu bar change when clicked

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
+// Get button and toggle menu when clicked
 let button = document.querySelector(".toggle-button");
-
 button.addEventListener('click', toggleNavMenu);
 
 function toggleNavMenu() {
     let navLinks = document.querySelector(".navbar-links");
     navLinks.classList.toggle('active')
+
+    // Change burger menu into X 
+    button.classList.toggle('change');
+};
+
+// Hides menu when clicked on something that's not the menu
+let main = document.querySelector(".main");
+main.addEventListener('click', closeNavMenu);
+
+function closeNavMenu() {
+    let navLinks = document.querySelector(".navbar-links");
+    navLinks.classList.remove('active');
+    button.classList.toggle('change');
 };
